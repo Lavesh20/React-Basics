1a1c4ea4fb17121418f812583413341e9249649a
unknown <laveshvyas20@gmail.com>
2024-02-26 12:28:08 +0530

    init

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [todos, setTodos] = useState([]);
 
+   useEffect(()=>{
+    setInterval(()=>{
+      fetch("https://sum-server.100xdevs.com/todos")
+      .then(async(res)=>{
+       const json =await  res.json();
+       setTodos(json.todos);
+      })
+      },10000,[])
+    })
+  
+   
   return (
-    <>
+    
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {todos.map(function(todo){
+       return <Todos key={todo.id} title = {todo.title} description = {todo.description}></Todos>
+      })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+    
   )
+  function Todos({title,description}){
+   return (
+    <div>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+    </div>
+  
+   )
+}
 }
 
 export default App
