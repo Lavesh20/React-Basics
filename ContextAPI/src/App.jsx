cfc2a31a34d013ddeae82c51a53ae12c57223971
unknown <laveshvyas20@gmail.com>
2024-03-02 18:33:23 +0530

    init

---
+++ ContextAPI/src/App.jsx
@@ -0,0 +1,44 @@
+import { useContext, useState } from "react"
+import { CountContext } from "./context"
+
+function App() {
+  const [count,setCount] = useState(0)
+
+  return (
+    <div>
+      <CountContext.Provider value={count}>
+      <Count count = {count} setCount={setCount}/>
+      </CountContext.Provider>
+       </div>
+  )
+
+}
+
+function CountRenderer(){
+  const count = useContext(CountContext);
+ return (
+  <div>
+    {count}
+  </div>
+ )
+}
+
+function Count({setCount}){
+  return (
+    <div>
+ <CountRenderer></CountRenderer>
+ <Button setCount = {setCount}/>
+    </div>
+  )
+}
+function Button({setCount}){
+  const count = useContext(CountContext);
+   return (
+    <div>
+      <button onClick={()=>{setCount(count+1)}}>Increase</button>
+      <button onClick={()=>{setCount(count-1)}}>Decrease</button>
+    </div>
+   )
+}
+
+export default App;
