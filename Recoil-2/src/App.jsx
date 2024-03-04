56ef0e5e8cbbfa423cabc3e86c690b98c7ca0ff2
unknown <laveshvyas20@gmail.com>
2024-03-04 22:17:26 +0530

    init

--- Recoil-2/src/App.jsx
+++ Recoil-2/src/App.jsx
@@ -1,16 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RecoilRoot, useRecoilValue } from "recoil"
+import { jobsAtom, messageAtom, newworkAtom, notificationAtom, totalNotificationSelector } from "./atoms"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <RecoilRoot>
+        <MainApp></MainApp>
+      </RecoilRoot>
+    </div>
+  )
+ 
+}
+
+function MainApp(){
+ const networkNotificationCount = useRecoilValue(newworkAtom);
+ const jobNotificationCount = useRecoilValue(jobsAtom);
+ const messageNotificationCount = useRecoilValue(messageAtom);
+ const notificationCount = useRecoilValue(notificationAtom);
+// const finalValue = networkNotificationCount +  jobNotificationCount + messageNotificationCount + notificationCount
+const totalNotificationValue = useRecoilValue(totalNotificationSelector);
 
   return (
     <>
-      hello
+      <button>Home</button>
+
+      <button>My network ({networkNotificationCount >=100 ? "99+":networkNotificationCount})</button>
+      <button>Jobs ({jobNotificationCount >=100 ? "99+":jobNotificationCount})</button>
+      <button>Messaging ({messageNotificationCount >=100 ? "99+" : messageNotificationCount})</button>
+      <button>Notifications ({notificationCount >=100 ? "99+" : notificationCount})</button>
+      
+      <button>Me ({totalNotificationValue})</button>
+
     </>
   )
+    
+  
 }
 
 export default App
---
+++ Recoil-2/src/atoms.js
@@ -0,0 +1,32 @@
+import { atom, selector } from "recoil";
+
+export const  newworkAtom = atom({
+    key: "networkAtom",
+    default:102
+});
+
+export const jobsAtom = atom({
+    key: "jobsAtom",
+    default:0
+});
+
+export const messageAtom = atom({
+    key: "messageAtom",
+    default:0
+});
+
+export const notificationAtom = atom({
+    key: "notificationAtom",
+    default:12
+}); 
+
+export const totalNotificationSelector = selector({
+    key: "totalNotificationSelector",
+    get : ({get})=>{
+     const networkAtomCount = get(newworkAtom);
+     const jobsAtomCount = get(jobsAtom);
+     const messageAtomCount = get(messageAtom);
+     const notificationAtomCount = get(notificationAtom);
+     return networkAtomCount + jobsAtomCount + messageAtomCount + notificationAtomCount
+    }
+})
